fe4b5ed1929d71257866670f231621c6123d2530
İhsan Mirage <tezravasen@gmail.com>
2024-10-31 13:32:53 +0300

    feat: complete second page

--- test.rb
+++ test.rb
@@ -6,3 +6,31 @@ puts Math.sqrt(9) # KAREKÖK
 A = 4**2
 B = 3**2
 puts Math.sqrt(A + B) # KAREKÖK
+#
+def hi # Method Tanımlama
+  puts "Hello World"
+end
+
+def hi(name) # Değişkenlerle Method Kullanmak
+  puts "Hi #{name}"
+end
+hi("Burak")
+
+def hi(name = "World") # MEtholarda Değişkenler İçin Varsayılan Değer Kullanmak
+  puts "Hi #{name}"
+end
+hi
+hi("Burak")
+
+class Greeter # Class Tanımlama
+  def initialize(name = "World")
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}! how are you today?"
+  end
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+Greeter.new("Burak").say_hi
