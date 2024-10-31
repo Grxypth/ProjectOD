bed57d239fe859448173d03abc8bc5359687d91d
İhsan Mirage <tezravasen@gmail.com>
2024-10-31 14:56:23 +0300

    feat: complete basic data type tutorial

---
+++ basicdatatypes.rb
@@ -0,0 +1,20 @@
+puts 1 + 1
+puts 2 - 1
+puts 2 * 2
+puts 10 / 5
+puts 2**2
+puts 8 % 2
+puts 10 % 4
+puts 17 / 5.to_f
+puts 15 / 3.to_f
+puts 18 / 5.to_i
+puts 6.even?
+puts 7.odd?
+puts 8.odd?
+puts "Welcome" + "to" + "iWallet"
+puts "Welcome" << "to" << "iWallet"
+puts "Welcome".concat("to").concat("iWallet")
+puts "hello"[0]
+name = "burak".capitalize
+puts "Hello #{name}"
+puts name.upcase
